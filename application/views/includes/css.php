9ea8482482999cec0bdb6aa2557330e61dc0bd62
Leonidas Ferreira <leonidas.f.silva@hotmail.com>
2019-05-04 18:31:19 -0300

    fix css

--- application/views/includes/css.php
+++ application/views/includes/css.php
@@ -21,8 +21,7 @@
 
 <!--    <link href="--><?php //echo base_url(); ?><!--assets/fonts/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">       -->
 <link href="<?php echo base_url(); ?>assets/css/styles.css" type="text/css" rel="stylesheet">                                     <!-- Core CSS with all styles -->
-<!--<link href="--><?php //echo base_url(); ?><!--assets/css/sweetalert2.css" type="text/css" rel="stylesheet">
-
+<!--<link href="--><?php //echo base_url(); ?><!--assets/css/sweetalert2.css" type="text/css" rel="stylesheet"> -->
 <link href="<?php echo base_url(); ?>assets/plugins/jstree/dist/themes/avenger/style.min.css" type="text/css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/plugins/codeprettifier/prettify.css" type="text/css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/plugins/iCheck/skins/minimal/blue.css" type="text/css" rel="stylesheet">
