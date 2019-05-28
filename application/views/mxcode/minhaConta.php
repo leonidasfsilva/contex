d774d4f78bfe0fb89447cc0abfcfe0f8dd6dcb9b
Leonidas Ferreira <leonidas.f.silva@hotmail.com>
2019-05-27 22:24:42 -0300

    ajuste

--- application/views/mxcode/minhaConta.php
+++ application/views/mxcode/minhaConta.php
@@ -110,7 +110,7 @@
                     <div class="row">
                         <div class="form-group col-md-6">
                             <label for="email" class="font-weight-bold">Email *</label>
-                            <input type="text" class="form-control" id="email" name="email" value="<?php echo $dados->email; ?>"/>
+                            <input type="text" class="form-control" id="email" name="email" value="<?php echo $dados->email; ?>" disabled/>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="telefone" class="font-weight-bold">Telefone</label>
