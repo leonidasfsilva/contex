0d2a37e3051c50230e3fa47521fbdf540a545b27
Leônidas Ferreira <leonidas.f.silva@hotmail.com>
2022-10-04 11:05:04 -0300

    adjusting name of user to default value NULL on Log registers

--- application/controllers/Mxcode.php
+++ application/controllers/Mxcode.php
@@ -144,7 +144,7 @@ class Mxcode extends CI_Controller
         $this->form_validation->set_rules('senha', 'Senha', 'required|trim');
 
         if ($this->form_validation->run() == false) {
-//            print_array(validation_errors());
+        //    print_array(validation_errors());
             $msgValidation = validation_errors();
             $this->session->set_flashdata('erro', 'Formato de e-mail inválido.');
             redirect('mxcode/login');
@@ -182,7 +182,7 @@ class Mxcode extends CI_Controller
                     redirect('mxcode/login');
                 }
             } else {
-                gravaLog(null, 'Usuário desconhecido', $email, 'Tentativa de login recusada: email inexistente', getenv("REMOTE_ADDR"));
+                gravaLog(null, null, $email, 'Tentativa de login recusada: email inexistente', getenv("REMOTE_ADDR"));
                 $this->session->set_flashdata('erro', 'Dados de acesso inválidos, por favor tente novamente.');
                 redirect('mxcode/login');
             }
