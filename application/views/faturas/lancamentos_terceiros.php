fd4fc42f2f559e6eeff96ebee910b245c584afba
Leônidas Ferreira <leonidas.f.silva@hotmail.com>
2022-10-04 22:25:58 -0300

    adjusting JS and other layouts

--- application/views/faturas/lancamentos_terceiros.php
+++ application/views/faturas/lancamentos_terceiros.php
@@ -1,8 +1,34 @@
+<?php
+$prevLink       = null;
+$nextLink       = null;
+$referenceText  = null;
+
+if (isset($referenceMonth) && $referenceMonth) {
+    $prevReferenceMonth = $referenceMonth - 1;
+    $nextReferenceMonth = $referenceMonth + 1;
+    $prevReferenceYear  = $referenceYear;
+    $nextReferenceYear  = $referenceYear;
+
+    if ($referenceMonth == '12') {
+        $nextReferenceMonth = '01';
+        $nextReferenceYear  = $referenceYear + 1;
+    }
+    if ($referenceMonth == '01') {
+        $prevReferenceMonth = '12';
+        $prevReferenceYear  = $referenceYear - 1;
+    }
+    $prevLink       = "<a href=" . base_url(sprintf('financeiro/faturas/terceiros?mesReferencia=%s&anoReferencia=%s&cartao=%s&nome=%s', $prevReferenceMonth, $prevReferenceYear, $idCard, $name)) . " title='Período anterior'><span class='badge badge-primary' style='margin-left: 10px;'><i style='margin: 0 !important;' class='fas fa-angle-double-left'></i></span></a>";
+    $referenceText  = "<span class='badge badge-primary' style='margin-left: 10px;'>Referência: $referencePeriod</span>";
+    $nextLink       = "<a href=" . base_url(sprintf('financeiro/faturas/terceiros?mesReferencia=%s&anoReferencia=%s&cartao=%s&nome=%s', $nextReferenceMonth, $nextReferenceYear, $idCard, $name)) . " title='Próximo período'><span class='badge badge-primary' style='margin-left: 10px;'><i style='margin: 0 !important;' class='fas fa-angle-double-right'></i></span></a>";
+}
+
+?>
 <div class="panel panel-midnightblue">
     <div class="panel-heading">
         <h2>
             Registro de Compras: <?= $name ?>
-            <?= (isset($referencePeriod) && $referencePeriod ? "<span class='badge badge-primary' style='margin-left: 10px;'>Referência: $referencePeriod</span>" : null) ?>
+            <br class="visible-xs-block">
+            <?= ($referenceMonth ? $prevLink . $referenceText . $nextLink : null) ?>
         </h2>
 
         <div class="panel-ctrls">
