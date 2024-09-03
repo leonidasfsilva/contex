7dfdb173d3f5d301d13ea7a1ebe7eb9b4359ac7a
Leônidas Ferreira <leonidas.f.silva@hotmail.com>
2024-09-03 13:28:23 -0300

    adding one more date option for card invoice

--- application/config/constants.php
+++ application/config/constants.php
@@ -19,7 +19,7 @@ include APPPATH . 'helpers' . DIRECTORY_SEPARATOR . 'codegen_helper.php';
  * | R     - current release version of system
  * |
  */
-$version       = '34';
+$version       = '35';
 $quarterOfYear = getQuarterOfCurrentYear();
 $currentYear   = date('Y');
 define("VERSION_APP", sprintf('%s.%s.%s', $currentYear, $quarterOfYear, $version));
