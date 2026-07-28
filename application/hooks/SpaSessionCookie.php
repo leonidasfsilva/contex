885e4b87fb54020cfe1110d6776df9402838a46c
Leônidas Ferreira <leonidas.f.silva@hotmail.com>
2026-07-27 22:47:17 -0300

    fix: remove cookie legado da sessao spa

--- application/hooks/SpaSessionCookie.php
+++ application/hooks/SpaSessionCookie.php
@@ -10,7 +10,27 @@ function selectSpaSessionCookie()
     $routePath  = ltrim(substr($requestUri, strlen($basePath)), '/');
 
     if (strpos($routePath, 'api/v1/') === 0) {
+        removeLegacySpaSessionCookie();
         $GLOBALS['CFG']->set_item('sess_cookie_name', 'api_session');
-        $GLOBALS['CFG']->set_item('sess_expiration', 600);
+        $GLOBALS['CFG']->set_item('sess_expiration', 60);
     }
+}
+
+function removeLegacySpaSessionCookie()
+{
+    if (!isset($_COOKIE['spa_session'])) {
+        return;
+    }
+
+    $cookieOptions = array(
+        'expires'  => 1,
+        'path'     => config_item('cookie_path') ?: '/',
+        'domain'   => config_item('cookie_domain') ?: '',
+        'secure'   => (bool)config_item('cookie_secure'),
+        'httponly' => true,
+        'samesite' => config_item('sess_samesite') ?: 'Lax',
+    );
+
+    setcookie('spa_session', '', $cookieOptions);
+    unset($_COOKIE['spa_session']);
 }
